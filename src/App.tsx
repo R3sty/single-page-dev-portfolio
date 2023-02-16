4fe34407d596c43c435f0142c39d68aeb2062ec3
R3sty <iral.resty@gmail.com>
2023-02-16 09:51:37 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-xl text-pink-500">
+App works with tailwindcss
+    </div>
+  )
+}
+
+export default App
